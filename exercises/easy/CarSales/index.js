7a8b9ff70418622b903d00947a1af053cf8f0176
“v3nd1x” <kristiansgerhards@gmail.com>
2019-08-06 17:19:37 +0300

    Added easy exercises for review

---
+++ exercises/easy/CarSales/index.js
@@ -0,0 +1,26 @@
+/**
+ * Car Sales
+ *
+ * Array of car sales information object is passed, your task is to calculate total sales for each of the make.
+ *
+ * Car sales information is an object with the following properties:
+ *
+ *  - make
+ *  - model
+ *  - colour
+ *  - price
+ */
+
+function sales(carsSold) {
+    let totals = {}
+    for (let i in carsSold){
+        let car = carsSold[i]
+        if (totals[car.make] == undefined){
+            totals[car.make] = 0 
+        }
+        totals[car.make] = totals[car.make] + car.price
+    }
+    return totals
+}
+
+module.exports = sales;
---
+++ exercises/easy/FormatCities/index.js
@@ -0,0 +1,12 @@
+/**
+ * Format Cities
+ *
+ * Array of countries and their capitals are passed to your function, together with a mapping function. Apply mapping function to an array and return newly created array as result.
+ */
+
+function formatCities(array, fn) {
+    return array.map(fn)
+
+}
+
+module.exports = formatCities;
